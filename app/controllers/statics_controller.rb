8d7759bbbf55995b0f5c6253d6a6b78d3b8f409b
fess <fess@fess.org>
2012-10-25 12:24:14 -0700

    shuffle the images

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,4 +1,10 @@
 class StaticsController < ApplicationController
   def index
+    @images = Hash[*([
+      ['test_1.png', "Alt text for test image #1 - PNG "],
+      ['test_2.gif', "Alt text for test image #2 - GIF "],
+      ['test_3.jpg', "Alt text for test image #3 - JPG "],
+      ['test_4.jpg', "Alt text for test image #4 - JPG "]
+    ].shuffle.flatten)]
   end
 end
